a78dc270d1509086115b47f405a362c275fa4205
hjk30 <dete_moe@mail.ru>
2023-02-24 14:41:00 +0700

    Lab1

---
+++ src/Organization.cs
@@ -0,0 +1,47 @@
+﻿using LAB1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    internal class Organization
+    {
+        private string _name;
+        private int _year;
+        private person _director;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Year
+        {
+            get { return _year; }
+            set {
+                VariableChecker.AssertPastYear(value);
+                _year = value;
+            }
+        }
+        public person Director
+        {
+            get { return _director; }
+            set { _director = value; }
+        }
+        public Organization(string name, int year, person director)
+        {
+            Name = name;
+            Year = year;
+            Director = director;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Организация " + Name + " основана в " + Year + " году, директор - " + Director);
+        }
+    }
+}
---
+++ src/person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    internal class person
+    {
+        private string _name_surname;
+        private int _gender;
+        private int _age;
+        public string NameSurname
+        {
+            get { return _name_surname; }
+            set {
+                VariableChecker.AssertAlphabetString(value);
+                _name_surname = value;
+            }
+        }
+        public int Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set {
+                VariableChecker.AssertPositiveNumber(value);
+                _age = value;
+            }
+        }
+
+        public virtual void Print()
+        {
+            string gender = "муж.";
+            if (Gender == 1) gender = "жен.";
+            Console.WriteLine(NameSurname + " возраст " + Age + " пол " + gender);
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите ФИО");
+            NameSurname = (Console.ReadLine());
+            Console.WriteLine("Введите пол (м - 0, ж - 1)");
+            Gender = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите возраст");
+            Age = int.Parse(Console.ReadLine());
+        }
+        public person(string name_surname = "DefaultName", int gender = 0, int age = 0)
+        {
+            NameSurname = name_surname;
+            Gender = gender;
+            Age = age;
+        }
+        ~person()
+        {
+            //empty destructor
+        }
+        private void AssertAge(int age)
+        {
+            if (age < 0) throw new ArgumentOutOfRangeException("Negative Age");
+        }
+    }
+}
